55a844c3762548820ee445596cb974b60c54fb94
germanps <gpla@pasiona.com>
2018-06-22 14:15:22 +0200

    component location

---
+++ src/components/location/Location.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import './Location.css';
+
+const Location = () => (
+   <div className="wa-location">
+      <h3>Bilbao</h3>
+   </div>
+);
+
+export default Location;
--- src/components/weatherLocation/WeatherLocation.js
+++ src/components/weatherLocation/WeatherLocation.js
@@ -1,9 +1,11 @@
 import React from 'react';
+import Location from './../location/Location';
 import './WeatherLocation.css';
 
 const WheatherLocation = () => (
-   <div className="col-md-9 wa-main-content">
-      <h2>WeatherLocation</h2>
+   <div className="col-md-9 wa-weather-location">
+      <h2>Weather App</h2>
+      <Location />
    </div>
 );
 
