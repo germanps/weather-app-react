f1488df26627f26394026e95d7cfa5a397403e31
Germán Pla <germanps@outlook.es>
2018-06-27 11:45:44 +0200

    click event => state change

--- src/components/weatherLocation/WeatherLocation.js
+++ src/components/weatherLocation/WeatherLocation.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import Location from './../location/Location';
 import WeatherData from './../weatherData/WeatherData';
-import { SUN } from './../../constants/weathers';
+import { SUN, WINDY } from './../../constants/weathers';
 import './WeatherLocation.css';
 
 const data = {
@@ -10,13 +10,38 @@ const data = {
     humidity: 60,
     wind: '20 m/s'
 }
+const data2 = {
+    temperature: 50,
+    weatherState: WINDY,
+    humidity: 20,
+    wind: '50 m/s'
+}
 class WheatherLocation extends Component{
-    render = () => (
-        <div className="wa-weather-location">
-            <Location city={'Bilbao'}/>
-            <WeatherData data={data}/>
-        </div>
-    );
+    constructor(){
+        super();
+        this.state = {
+            city: 'Bilbao',
+            data: data
+        }
+    }
+    handleUpdateClick = () => {
+        this.setState({
+            city: 'Bilbao',
+            data: data2
+        });
+        console.log('Actualizado');
+    }
+    render = () => {
+        const { city, data } = this.state; //destructuring
+       
+        return(        
+            <div className="wa-weather-location">
+                <Location city={city}/>
+                <WeatherData data={data}/>
+                <button onClick={this.handleUpdateClick}>Actualizar</button>
+            </div>
+        )
+    };
 }
 
 export default WheatherLocation;
